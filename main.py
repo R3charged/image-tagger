a8e9224df266cf222cdad1affbc891757f9a7c60
R3charged <leerichardjin@gmail.com>
2023-10-14 00:16:43 -0400

    adds image_to_prompt function

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PIL import Image
 from clip_interrogator import Config, Interrogator
 
-print("hello world")
-image = Image.open(image_path).convert('RGB')
-ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
-print(ci.interrogate(image))
+def image_to_prompt(image):
+    ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
+    return ci.interrogate(image)
