dcd30aa24ff69c709b9dc4836026a20c499803ca
Rich-Hoepfinger <richhoepfinger@gmail.com>
2023-10-13 23:27:51 -0400

    Debug deletion

--- keys.py
+++ keys.py
@@ -4,7 +4,6 @@ def extract_keywords(sentence):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(sentence)
     tags = [tag for tag in doc.ents]
-    print(tags)
     return tags
     
 def trim_and_hash(tags):
@@ -18,8 +17,3 @@ def trim_and_hash(tags):
         
 def get_keywords_from_sentence(sentence):
     return trim_and_hash(extract_keywords(sentence))
-        
-    
-get_keywords_from_sentence("""spaCy is an open-source software library for advanced natural language processing, 
-        written in the programming languages Python and Cython. The library is published under the MIT license
-        and its main developers are Matthew Honnibal and Ines Montani, the founders of the software company Explosion.""")
