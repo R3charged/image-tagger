13df00eacbc6280426223ab259cfcb5ec24c74f7
R3charged <leerichardjin@gmail.com>
2023-10-13 21:26:01 -0400

    Environment Setup

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+from PIL import Image
+from clip_interrogator import Config, Interrogator
+
+print("hello world")
+image = Image.open(image_path).convert('RGB')
+ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
+print(ci.interrogate(image))
