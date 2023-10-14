c31947e0564cd41a3af3cd4cd165c5b16d7e5ea9
Rich-Hoepfinger <richhoepfinger@gmail.com>
2023-10-13 22:34:07 -0400

    Added # to start and removed spaces

--- keys.py
+++ keys.py
@@ -3,10 +3,23 @@ import spacy
 def extract_keywords(sentence):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(sentence)
-    print(doc.ents)
-    return doc.ents
+    tags = [tag for tag in doc.ents]
+    print(tags)
+    return tags
     
-
-extract_keywords("""spaCy is an open-source software library for advanced natural language processing, 
+def trim_and_hash(tags):
+    for idx, tag in enumerate(tags):
+        tag_str = tag.text
+        tag_str = f"#{tag_str}"
+        tag_str = tag_str.replace(" ", "")
+        tags[idx] = tag_str
+    return tags
+        
+        
+def get_keywords_from_sentence(sentence):
+    return trim_and_hash(extract_keywords(sentence))
+        
+    
+get_keywords_from_sentence("""spaCy is an open-source software library for advanced natural language processing, 
         written in the programming languages Python and Cython. The library is published under the MIT license
         and its main developers are Matthew Honnibal and Ines Montani, the founders of the software company Explosion.""")
